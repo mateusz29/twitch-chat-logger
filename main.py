0c3260bc036a926ac063e1233d432feee84d1d0b
mateusz29 <strzeleckimateusz10@gmail.com>
2024-09-07 19:33:15 +0200

    Made seperate module for handling twitch api

--- main.py
+++ main.py
@@ -1,85 +1,12 @@
 import asyncio
-import json
 import os
 
-import requests
 import twitchio
 from dotenv import load_dotenv
 from twitchio.ext import commands
+from twitch_api import get_category_id, get_live_channels
 
 load_dotenv()
-
-
-def get_category_id(category_name: str, client_id: str, oauth_token: str) -> str:
-    url = "https://api.twitch.tv/helix/games"
-    headers = {"Client-ID": client_id, "Authorization": f"Bearer {oauth_token}"}
-    params = {"name": category_name}
-
-    response = requests.get(url, headers=headers, params=params)
-    data = response.json()
-
-    if not data["data"]:
-        raise ValueError(f"No category found with name '{category_name}'")
-
-    return data["data"][0]["id"]
-
-
-def get_live_channels(category_id: str, client_id: str, oauth_token: str) -> list[str]:
-    url = "https://api.twitch.tv/helix/streams"
-    headers = {"Client-ID": client_id, "Authorization": f"Bearer {oauth_token}"}
-    params = {
-        "game_id": category_id,
-        "first": 100,  # maximum number of results per request
-    }
-
-    response = requests.get(url, headers=headers, params=params)
-    channels = response.json()["data"]
-
-    return [channel["user_login"] for channel in channels]
-
-
-def send_chat_message(
-    broadcaster_id: str, sender_id: str, message: str, client_id: str, oauth_token: str
-) -> None:
-    url = "https://api.twitch.tv/helix/chat/messages"
-    headers = {
-        "Authorization": f"Bearer {oauth_token}",
-        "Client-Id": client_id,
-        "Content-Type": "application/json",
-    }
-    payload = {
-        "broadcaster_id": broadcaster_id,
-        "sender_id": sender_id,
-        "message": message,
-    }
-
-    response = requests.post(url, headers=headers, data=json.dumps(payload))
-
-    if response.status_code == 200:
-        print("Message sent successfully!")
-    else:
-        raise Exception(
-            f"Failed to send message: {response.status_code} - {response.text}"
-        )
-
-
-def get_user_id(username: str, client_id: str, oauth_token: str) -> str:
-    url = "https://api.twitch.tv/helix/users"
-    headers = {"Authorization": f"Bearer {oauth_token}", "Client-Id": client_id}
-    params = {"login": username}
-
-    response = requests.get(url, headers=headers, params=params)
-
-    if response.status_code == 200:
-        data = response.json()
-        if data["data"]:
-            return data["data"][0]["id"]
-        else:
-            raise ValueError(f"User {username} not found")
-    else:
-        raise Exception(
-            f"Failed to fetch user ID: {response.status_code} - {response.text}"
-        )
 
 
 class Bot(commands.Bot):
